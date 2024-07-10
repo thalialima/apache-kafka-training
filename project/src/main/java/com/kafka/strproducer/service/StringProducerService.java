e701db63e7eff245cfd04296974f5bd30844ef67
Thalia Lima <thaliairene20@gmail.com>
2024-07-10 11:52:26 -0300

    Add callback function for logs

--- project/src/main/java/com/kafka/strproducer/service/StringProducerService.java
+++ project/src/main/java/com/kafka/strproducer/service/StringProducerService.java
@@ -1,18 +1,33 @@
 package com.kafka.strproducer.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+
 @Service
 public class StringProducerService {
+
+    private static final Logger LOG = LogManager.getLogger(StringProducerService.class);
+
 
     @Resource
     public KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if (success != null) {
+                        LOG.info("Message: {} sent with success.", message);
+                        LOG.info("Partition: {}", success.getRecordMetadata().partition());
+                        LOG.info("Offset: {}", success.getRecordMetadata().offset());
+                    }
+                },
+                error -> LOG.error("Error while sending message!")
+        );
     }
 
 }
