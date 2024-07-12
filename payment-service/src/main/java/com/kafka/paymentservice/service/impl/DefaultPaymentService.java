42ea96254dc45182c1656550b9c8c35009db1c82
Thalia Lima <thaliairene20@gmail.com>
2024-07-12 08:48:37 -0300

    Sending objects for kafka

--- payment-service/src/main/java/com/kafka/paymentservice/service/impl/DefaultPaymentService.java
+++ payment-service/src/main/java/com/kafka/paymentservice/service/impl/DefaultPaymentService.java
@@ -4,8 +4,11 @@ import com.kafka.paymentservice.model.Payment;
 import com.kafka.paymentservice.service.PaymentService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.io.Serializable;
 
 
 @Service
@@ -13,9 +16,20 @@ public class DefaultPaymentService implements PaymentService {
 
     private static final Logger LOG = LogManager.getLogger(DefaultPaymentService.class);
 
+    @Resource
+    private KafkaTemplate<String, Serializable> kafkaTemplate;
+
     @Override
     public void sendPayment(Payment payment) {
-        LOG.info("PAYMENT_SERVICE Payment received: {}", payment);
+        try {
+            LOG.info("Payment received: {}", payment);
+            Thread.sleep(1000);
+            kafkaTemplate.send("payment-topic", payment);
+            LOG.info("Sending payment...");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            LOG.error("Error during send payment, due to: {}", e.getMessage());
+        }
 
     }
 }
